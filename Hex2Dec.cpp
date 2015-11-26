c16b30d1101218887d34ccded68da73a92666fe3
sugarfreeva <asweet1983@gmail.com>
2015-11-26 10:00:55 -0500

    Opening File check

--- Hex2Dec.cpp
+++ Hex2Dec.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 int main(int argc, char* argv[])
 {
+    std::fstream hexFile(argv[1], std::ios::in);
+
+    if(hexFile.is_open() == true)
+    {
+        
+    }
+    else
+    {
+        std::cout << "ERROR OPENING HEX FILE!" << std::endl;
+    }
+
     return 0;
 }
